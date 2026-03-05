5be679cc85d50827854c74630eed2da4e59fb6a8
Kostya Vakarian <vakarianplay@gmail.com>
2026-03-05 12:53:59 +0300

    Update weather API key placeholder in AppConfig

--- ESP-ntpclock/src/AppConfig.h
+++ ESP-ntpclock/src/AppConfig.h
@@ -4,6 +4,6 @@
 struct AppConfig {
     String ntpServer = "ntp1.vniiftri.ru";
     String weatherCity = "Moscow";
-    String weatherApiKey = "28bc310f78d0674674d5ca06e7a2a556";
+    String weatherApiKey = "openweather api key here";
     int32_t tzOffsetSec = 3 * 3600;
 };
