48be0294b773ee17a7aadd84d3952e088042e771
Kostya Vakarian <vakarianplay@gmail.com>
2026-03-09 21:27:21 +0300

    Finally commit

--- ESP-ntpclock/src/main.cpp
+++ ESP-ntpclock/src/main.cpp
@@ -78,7 +78,7 @@ void setup() {
     lcd.setCursor(0, 0);
     lcd.print("Connecting...");
 
-    wifiManager.autoConnect("ESP Connect");
+    wifiManager.autoConnect("Sonoff Connect");
 
     webServer.begin();
     webServer.setOnSave(applyConfig);
@@ -103,5 +103,4 @@ void loop() {
         drawScroll();
         scrollPos = (scrollPos + 1) % max<uint16_t>(1, scrollText.length());
     }
-
 }
